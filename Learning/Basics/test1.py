ba0482085fdcec21a02ab1095f6dc228f47bdb7e
Ashirul Ansari <77456634+ashirul@users.noreply.github.com>
2021-11-19 17:09:53 +0530

    Add files via upload

---
+++ Learning/Basics/test1.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello There!")
+    name = input("What is your Name ")
+    print("Nice to meet you, ", name)
+
+if __name__ == "__main__":
+    main()
---
+++ Learning/Basics/variables.py
@@ -0,0 +1,9 @@
+f=0
+#print(f)
+
+def someFunction():
+    f="def"
+    print(f)
+
+someFunction()
+print(f)
